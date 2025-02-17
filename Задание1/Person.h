ac66ea7164e97daed8cc11a63c49807e2c0bbc68
VelichkoIS <130165421+VelichkoIS@users.noreply.github.com>
2025-02-17 20:11:20 +0300

    Create Person.h

---
+++ Задание1/Person.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class Person
+{
+private:
+	string lastName;
+	string firstName;
+	string patronymic;
+public:
+	Person();
+	Person(const string& ln, const string& fn, const string& pn = "");
+	void Show(); //lastName + firstName
+	void ShowFormal(); // ФИО
+};
