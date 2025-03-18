ffa5c6b4546ebad862f2882f691fd0647c0173ba
VelichkoIS <velichkois20@gmail.com>
2025-03-18 18:09:40 +0300

    Первоночальная настройка решения

---
+++ A regular polygon/Test.Solver/Test.Solver.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestSolver
+{
+	TEST_CLASS(TestSolver)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
