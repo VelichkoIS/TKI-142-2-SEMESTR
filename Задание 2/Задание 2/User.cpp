e77cf50e7cc65cfc717a8c215f42897f24b35ae6
VelichkoIS <velichkois20@gmail.com>
2025-03-31 13:06:05 +0300

    Удалил файлы

--- Задание 2/Задание 2/User.cpp
+++
@@ -1,9 +0,0 @@
-#include "User.h"
-
-double User::input(std::string messege)
-{
-	std::cout << messege;
-	double value = 0.0;
-	std::cin >> value;
-	return value;
-}
--- Задание 2/Задание 2/User.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-
-class User
-{
-public:
-	double input(std::string messege);
-};
